6704f65c3f28f6f5c716fb98ea08af1b6e8f24de
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-09-27 00:12:00 +0200

    switch to wget

--- get-spec.sh
+++ get-spec.sh
@@ -7,5 +7,5 @@ mkdir -p /docs/docs/spec/heads
 
 # Loop through each version
 for version in "tags/0.1" "tags/0.2" "tags/0.3" "heads/main" ; do
-  curl -sS -L -o "/docs/docs/spec/$version.md" "https://raw.githubusercontent.com/AssetFetch/spec/refs/$version/spec.md"
+  wget -O "/docs/docs/spec/$version.md" "https://raw.githubusercontent.com/AssetFetch/spec/refs/$version/spec.md"
 done
