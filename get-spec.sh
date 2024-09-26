2b7e41f4439356ffbf31e493ecce5f5d7e0f1d1a
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-09-27 00:09:14 +0200

    Update get-spec.sh

--- get-spec.sh
+++ get-spec.sh
@@ -2,8 +2,10 @@
 
 # Remove existing files
 rm -rf -v /docs/docs/spec/*
+mkdir -p /docs/docs/spec/tags
+mkdir -p /docs/docs/spec/heads
 
 # Loop through each version
 for version in "tags/0.1" "tags/0.2" "tags/0.3" "heads/main" ; do
-  curl --create-dirs -sS -L -o "/docs/docs/spec/$version.md" "https://raw.githubusercontent.com/AssetFetch/spec/refs/$version/spec.md"
+  curl -sS -L -o "/docs/docs/spec/$version.md" "https://raw.githubusercontent.com/AssetFetch/spec/refs/$version/spec.md"
 done
