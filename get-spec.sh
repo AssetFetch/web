7cc2e48fa5ab7ee6af013e5ae7e33ee79c84385a
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-09-30 20:43:23 +0200

    Show v0.4 of the spec

--- get-spec.sh
+++ get-spec.sh
@@ -6,6 +6,6 @@ mkdir -p /docs/docs/spec/tags
 mkdir -p /docs/docs/spec/heads
 
 # Loop through each version
-for version in "tags/0.1" "tags/0.2" "tags/0.3" "heads/main" ; do
+for version in "tags/0.1" "tags/0.2" "tags/0.3" "tags/0.4" "heads/main" ; do
   wget -O "/docs/docs/spec/$version.md" "https://raw.githubusercontent.com/AssetFetch/spec/refs/$version/spec.md"
 done
